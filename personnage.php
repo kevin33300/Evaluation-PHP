fd2058912fdc3ea8afc8ef9d874983da8028989a
kevin <kevinfrance17440@gmail.com>
2022-01-03 16:43:29 +0100

    👌 IMPROVE: Ajout des méthodes dans le projet

--- personnage.php
+++ personnage.php
@@ -35,12 +35,57 @@
         // Création de la méthode permettant la description du personnage créé
         public function description_personnage() {
             echo " Votre personnage créé s'appelle ". $this->name . " ! \n";
-            echo $this->name. " dispose de " . $this->attack ." points d'attaque, ". $this->defense .  " points de défense et " .$this->mana . "points de mana ! Vous n'avez pas d'items pour l'instant. ";
+            echo $this->name. " dispose de " . $this->attack ." points d'attaque, ". $this->defense .  " points de défense et " .$this->mana . "points de mana ! Vous n'avez pas d'items pour l'instant. \n";
 
         }
 
+        // Création des méthodes pour afficher les caractéristiques du joueur en cours de Jeu (perfectible mais je ne connais pas encore la méthode via match)
+
+        public function getHealthPoint() {
+            return "Vous avez ". $this->health_point ." points de santé . \n"; 
+        }
+
+        public function getAttackPoint() {
+            return "Vous avez ". $this->attack ." points dattaque .\n " ;
+        }
+
+        
+
+        public function getDefensePoint() {
+            return "Vous avez ". $this->defense ." points de défense . \n" ;
+        }
+
+        public function getManaPoint() {
+            return "Vous avez ". $this->mana  ." points de mana . \n" ;
+        }
+
+        public function getItems() {
+            return $this->items;
+        }
+
+        //  Création d'une méthode pour ajouter un objet dans la liste items
+
+        public function setItems($item) {
+            array_push($this->items,$item);
+            echo "Vous avez ajouté un ".$item . " à vos items ";
+        }
+
+        //  Création d'une méthode pour rechercher si l'on possède un item 
+        public function searchItem($item) {
+            if (array_search($item, $this->items)) {
+                echo " Vous possédez bien un ". $item ." dans votre sac . ";
+            }
+        }
+
+        //  Création d'une méthode pour supprimer un item 
+        public function removeItem ($item) {
+            unset($this->items[array_search($item,$this->items)]);
+        }
+
+
+
        
-    }
+}
 
 
         
--- test.php
+++ test.php
@@ -8,6 +8,36 @@
    // Affichage des caractéristiques du personnag via une méthode 
    $personnage1 -> description_personnage();
 
+   
+
+    // affichage des points de vie:
+    echo $personnage1->getHealthPoint();
+
+    // affichage des points d'attaque':
+    echo $personnage1->getAttackPoint();
+
+    // affichage des points de mana:
+    echo $personnage1->getManaPoint();
+
+    // affichage des points des items:
+    var_dump($personnage1->getItems()) ;
+
+    // ajout d'un item et vérification du tableau 
+    $personnage1->setItems("couteau");
+    var_dump($personnage1->getItems()) ;
+
+    // Vérification via une méthode si l'item ajouté se trouve bien dans le sac. 
+    $personnage1->searchItem("couteau");
+
+    // Suppression d'un item et vérification du tableau 
+    $personnage1->removeItem("couteau");
+    var_dump($personnage1->getItems()) ;
+
+    
+
+    
+
+
  
 ?>
 
